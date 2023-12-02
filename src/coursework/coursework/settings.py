a09dd5d0fc1876a82d132f5363c4e26d5fc0cf7d
Andrew925 <andry925@ukr.net>
2023-12-02 20:15:10 +0200

    Added database configuration

--- src/coursework/coursework/settings.py
+++ src/coursework/coursework/settings.py
@@ -78,10 +78,13 @@ WSGI_APPLICATION = 'coursework.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'final_work',
+        'USER': 'root',
+        'PASSWORD': 'panel2004'
     }
 }
+
 
 
 # Password validation
